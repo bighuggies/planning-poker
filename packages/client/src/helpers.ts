daa8cd13998cfb67c54a82d9a24a6fed197d5109
adjohnston <adam.johnston3679@gmail.com>
2019-01-02 10:36:00 +0000

    a partial helper function

--- packages/client/src/helpers.ts
+++ packages/client/src/helpers.ts
@@ -1,3 +1,7 @@
 export const compose = (...fns: Function[]) => {
   return fns.reduce((fns, fn) => (value: Function) => fns(fn(value)))
 }
+
+export const partial = (fn: Function, ...values: any[]) => {
+  return fn.bind(null, ...values)
+}

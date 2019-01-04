3eedc302ba0e5763a58314313076504f62d49291
adjohnston <adam.johnston3679@gmail.com>
2019-01-04 20:04:47 +0000

    update card copy

--- packages/client/src/constants.js
+++ packages/client/src/constants.js
@@ -1,6 +1,7 @@
-module.exports = {
-  cards: {
-    fibonacci: [0, 1, 2, 3, 5, 8, 13, 21, 50, 100],
-    misc: ['BREAK', 'CANNOT_ESTIMATE'],
+export const cards = {
+  fibonacci: [0, 1, 2, 3, 5, 8, 13, 21, 50, 100],
+  misc: {
+    BREAK: '☕️',
+    CANNOT_ESTIMATE: '🤔',
   },
 }

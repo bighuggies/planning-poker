37ca893feeda29623ab615cb9ce2621cf58b5f6d
adjohnston <adam.johnston3679@gmail.com>
2019-01-12 14:31:48 +0000

    link components package and use some dependencies

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -4,6 +4,9 @@ import App from './components/containers/App/App'
 import { StateProvider } from './components/utils/WithState/WithState'
 import { ActionsProvider } from './components/utils/WithActions/WithActions'
 
+import '@planning-poker/components/dist/components.css'
+import './styles/index.css'
+
 render((
   <StateProvider>
     <ActionsProvider>
--- packages/client/src/react-app-env.d.ts
+++ packages/client/src/react-app-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="react-scripts" />
+
+declare module '@planning-poker/components'

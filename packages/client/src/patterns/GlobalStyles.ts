de047503b2a8f2a9b054e519e3d0081ab5ed5173
Andrew Hughson <andrew@hughson.me>
2019-04-22 01:07:46 +0100

    fix stylelint error

--- packages/client/src/patterns/GlobalStyles.ts
+++ packages/client/src/patterns/GlobalStyles.ts
@@ -1,10 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  *,*::after,*::before {
+  *, *::after, *::before {
     box-sizing: border-box;
   }
-
+  
   :root {
     font-size: 20px;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
